3e8d94198af1f9fc67dbe29350400c041718bc24
Dishant Singh <shahdilipkumar909@gmail.com>
2025-07-22 10:25:00 +0000

    fixed

--- api/src/redis.ts
+++ api/src/redis.ts
@@ -2,10 +2,18 @@ import { createClient } from 'redis';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const REDIS_URL = process.env.REDIS;
+const REDIS_HOST = process.env.REDIS_HOST; // e.g., "localhost:6379"
+const REDIS_PASS = process.env.REDIS_PASS;
 
-const client = createClient({ url: `redis://${REDIS_URL}` });
-const subscriber = createClient({ url: `redis://${REDIS_URL}` });
+const client = createClient({
+  url: `redis://${REDIS_HOST}`,
+  password: REDIS_PASS,
+});
+
+const subscriber = createClient({
+  url: `redis://${REDIS_HOST}`,
+  password: REDIS_PASS,
+});
 
 client.on('error', (err) => console.log('Redis Client Error', err));
 subscriber.on('error', (err) => console.log('Redis Subscriber Error', err));
