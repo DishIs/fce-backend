a586d06518af05760112143d90153ae9c0fdd50c
Dishant Singh <dishantsinghdev@gmail.com>
2025-08-10 08:57:22 +0000

    fixed redis host

--- api/src/redis.ts
+++ api/src/redis.ts
@@ -2,17 +2,14 @@ import { createClient } from 'redis';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const REDIS_HOST = process.env.REDIS_HOST; // e.g., "localhost:6379"
-const REDIS_PASS = process.env.REDIS_PASS;
+const REDIS_HOST = process.env.REDIS_HOST || 'localhost:6379';
 
 const client = createClient({
   url: `redis://${REDIS_HOST}`,
-  password: REDIS_PASS,
 });
 
 const subscriber = createClient({
-  url: `redis://${REDIS_HOST}`,
-  password: REDIS_PASS,
+  url: `redis://${REDIS_HOST}`
 });
 
 client.on('error', (err) => console.log('Redis Client Error', err));
